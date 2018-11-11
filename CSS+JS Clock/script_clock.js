68ea3608dc8fbd786a9b3a107d0b53c8f47513b3
Tunde Osborne <osbornetunde@gmail.com>
2018-11-11 23:38:25 +0100

    removed console.log()

--- CSS+JS Clock/script_clock.js
+++ CSS+JS Clock/script_clock.js
@@ -3,7 +3,6 @@ function setDate() {
 	const seconds = now.getSeconds();
 	const secondsDegrees = ((seconds / 60) * 360) + 90 ;
 	secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-	console.log(seconds);
 
 	const minutes = now.getMinutes();
 	const minutesDegrees = ((minutes / 60) * 360) + 90;
